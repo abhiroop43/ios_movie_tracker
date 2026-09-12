0efb7d8187be1918b11f725f0f877c9f25cfe124
Abhiroop Santra <abhiroop.santra@gmail.com>
2026-09-12 13:15:11 +0400

    Initial Commit

---
+++ MovieTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieTracker
+//
+//  Created by Abhiroop Santra on 12/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieTracker/MovieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackerApp.swift
+//  MovieTracker
+//
+//  Created by Abhiroop Santra on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
